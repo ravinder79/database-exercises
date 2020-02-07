262709375261689b07f3fa1b97aeb194ec6410bb
Ravinder Singh <ravinder79@gmail.com>
2020-02-07 15:39:36 -0600

    updated

--- 3.3_tables_exercises.sql
+++ 3.3_tables_exercises.sql
@@ -15,7 +15,7 @@ SHOW TABLES;
 DESCRIBE `employees`;
 
 #Which table(s) do you think contain a numeric type column?
-# Answer: Salaries, employees_with_departments, employees, dept_manager, dept_emp#
+# Answer: Salaries, employees_with_departments, employees, dept_manager, dept_emp, titles#
 SHOW TABLES;
 
 
@@ -26,9 +26,11 @@ SHOW TABLES;
 # dept_emp, dept_manager, employees, salaries, titles
 
 #What is the relationship between the employees and the departments tables?
-#no relationship
+DESCRIBE employees;
+DESCRIBE departments;
+#no direct relationship
 
 #SHOW the SQL that created the dept_manager table.
 SHOW CREATE TABLE dept_manager;
 
-#dept_manager	CREATE TABLE `dept_manager` (↵  `emp_no` int(11) NOT NULL,↵  `dept_no` char(4) NOT NULL,↵  `from_date` date NOT NULL,↵  `to_date` date NOT NULL,↵  PRIMARY KEY (`emp_no`,`dept_no`),↵  KEY `dept_no` (`dept_no`),↵  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,↵  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE↵) ENGINE=InnoDB DEFAULT CHARSET=latin1
+/*dept_manager	CREATE TABLE `dept_manager` (↵  `emp_no` INT(11) NOT NULL,↵  `dept_no` CHAR(4) NOT NULL,↵  `from_date` DATE NOT NULL,↵  `to_date` DATE NOT NULL,↵  PRIMARY KEY (`emp_no`,`dept_no`),↵  KEY `dept_no` (`dept_no`),↵  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,↵  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE↵) ENGINE=INNODB DEFAULT CHARSET=latin1*/
