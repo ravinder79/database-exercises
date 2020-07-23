0cbdd3187317b1c5dcb2c847b3a5a26681ed3019
Ravinder Singh <ravinder79@gmail.com>
2020-07-23 14:40:58 -0500

    add more exercises

--- sql_practice.sql
+++ sql_practice.sql
@@ -12,3 +12,43 @@ WHERE last_name LIKE 'E%';
 SELECT * FROM  employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
+/* Find all employees born on Christmas */
+
+SELECT * FROM employees
+WHERE birth_date LIKE '%%%%-12-25';
+
+/* Find all employees with a 'q' in their last name  */
+
+SELECT * FROM employees
+WHERE last_name LIKE '%q%';
+
+
+/* Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN */
+
+SELECT * FROM employees
+WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+
+/* Add a condition to the previous query to find everybody with those names who is also male */
+
+SELECT * FROM `employees`
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
+
+
+/* Find all employees whose last name starts or ends with 'E'  */
+
+SELECT * FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+/* Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' */
+
+SELECT * FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+/* Find all employees with a 'q' in their last name but not 'qu' */
+
+SELECT * FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
+
+/* Create a new file named order_by_exercises.sql and copy in the contents of your exercise from the previous lesson */
+
