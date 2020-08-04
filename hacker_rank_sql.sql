fabc53ed07921f04271d63420cc96c9604d53661
Ravinder Singh <ravinder79@gmail.com>
2020-08-04 14:16:10 -0500

    add code

--- hacker_rank_sql.sql
+++ hacker_rank_sql.sql
@@ -8,4 +8,22 @@ Write a query to help Eve.*/
 
 select if (grade > 7, name, NULL), GRADE, marks from STUDENTS
 JOIN grades on students.marks between Grades.Min_mark and Grades.Max_mark
-order by GRADE DESC, name, marks
+order by GRADE DESC, name, marks;
+
+/*
+Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! 
+Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. 
+Order your output in descending order by the total number of challenges in which the hacker earned a full score.
+ If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.*/
+
+
+select t.h_id, t.name
+from (
+SELECT submissions.hacker_id as h_id, name as name, count(submissions.hacker_id) as count1  from Submissions
+JOIN Challenges using (challenge_id)
+JOIN difficulty using (difficulty_level)
+JOIN Hackers on submissions.hacker_id = hackers.hacker_id
+where Difficulty.score = Submissions.score
+group by submissions.hacker_id, name
+order by count(submissions.hacker_id) DESC, submissions.hacker_id) as t
+where t.count1>1;
