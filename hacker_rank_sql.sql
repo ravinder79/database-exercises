3bc13d1079041fe405c47d621279d03fbb7e3e67
Ravinder Singh <ravinder79@gmail.com>
2020-09-24 21:59:36 -0500

    add exercise

--- hacker_rank_sql.sql
+++ hacker_rank_sql.sql
@@ -183,3 +183,8 @@ select city, length(city) from station
 order by length(city),city asc
 limit 1;
 
+
+/*Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.*/
+
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY REGEXP '^[^aeiou]|[^aeiou]$';
